2a601e992b6253048aee8eaf7af94b536537ca67
Paul Brady <pablo.brady@gmail.com>
2015-08-01 17:16:31 -0700

    Complete.  Find MIDDLE NODE.  Big 0 Linear (best case possible).

--- llistMiddleNode/javascript.js
+++ llistMiddleNode/javascript.js
@@ -39,8 +39,41 @@ LList.prototype.addNode = function(value) {
   console.log("addNode:  VALUE ", currNode);
 };
 
+LList.prototype.middleNode = function() {
+  if(!this.head) return null;
+
+  var counter = 0;
+  var mCounter = 0;
+
+  var currNode = this.head;
+  var midNode  = this.head;
+  while(currNode) {
+    currNode = currNode.next;
+    counter++;
+
+    if(counter%2 === 0) {
+      midNode = midNode.next;
+      mCounter++;
+    }
+  }
+
+  return midNode;
+};
+
 var myList = new LList();
 myList.addNode(1);
 myList.addNode(2);
 myList.addNode(3);
+myList.addNode(4);
+myList.addNode(5);
+console.log("MID NODE:  ", myList.middleNode());
+
+myList.addNode(6);
+console.log("MID NODE:  ", myList.middleNode());
+
+myList.addNode(7);
+console.log("MID NODE:  ", myList.middleNode());
+
+
+
 

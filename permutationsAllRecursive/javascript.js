42cacf086114cd82b40384e3ca23a25d44b3509f
Paul Brady <pablo.brady@gmail.com>
2015-10-08 16:23:14 -0700

    [Bugfix] Oops.  Better later than never, I guess.

--- permutationsAllRecursive/javascript.js
+++ permutationsAllRecursive/javascript.js
@@ -1,4 +1,4 @@
-console.log("Permutations, ALL (Recursive).");
+console.log("Show all unique Permutations of letters for each word. (Recursive)");
 
 var insert = function (str, index, value) {
   return str.substr(0, index) + value + str.substr(index);
@@ -6,37 +6,44 @@ var insert = function (str, index, value) {
 
 
 var getAllPermutations = function( inputStr ) {
-  var comboArray = [];
+  var comboObject = {};
+
 
   var recurse = function( inputStr, pos ) {
-    var mixedWords = [];
     var ch = inputStr[pos];
 
     // Base Case
     if(pos >= inputStr.length) {
-      return comboArray;
+      return comboObject;
     }
 
-
-    // Remove CH from inputStr.
+    // Remove char from inputStr.
     var rStr = inputStr.replace(ch, "");
-    console.log("BASIC WORD: " + rStr + ", CHAR: " + ch);
-    // Place CH in every possible location.
+
+    // Place char in every possible location.
     var len = rStr.length;
     for(var i=0; i<=len; i++) {
-      var subWord = insert(rStr, i, ch);
-      comboArray = comboArray.concat( subWord );
+      var subWord = "" +  insert(rStr, i, ch);
+      if(!comboObject[subWord]) {
+        comboObject[subWord] = true;
+      } else comboObject[subWord] = false;
     }
     pos++;
     recurse( inputStr, pos);
 
-    return comboArray;
+    return comboObject;
   };
+  recurse( inputStr, 0 );
 
-  console.log( recurse( inputStr, 0 ) );
 
-
-  return comboArray;
+  return comboObject;
 };
 
-console.log("OUTPUT:  ", getAllPermutations( "BIKE" ) );
+
+var origWord1 = "BYE";
+var outObj1 = getAllPermutations( origWord1 );
+console.log( Object.keys(outObj1) );
+
+origWord2 = "TUNAFISH";
+var outObj2 = getAllPermutations( origWord2 );
+console.log( Object.keys(outObj2) );

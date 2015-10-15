b048f6334953a3d33e9e0349947eeb93f6b0b03f
Paul Brady <pablo.brady@gmail.com>
2015-10-15 14:30:23 -0700

    [Refactor]  Optimized answer.

--- rand5/javascript.js
+++ rand5/javascript.js
@@ -9,29 +9,23 @@ var rand5viaRand7 = function( sampleSize ) {
   var r5Results = {};
   for(var k=1; k<=sampleSize; k++) {
     // var r7 = rand7() - 1; // New range:  0 to 6;
-    var r5 = Math.floor( 
-        ( 
-          ((rand7() - 1) * (100/7)) + 
-          ((rand7() - 1) * (14/7)) + 
-          ((rand7() - 1) / 3.5 )
-          ) / (100/5) 
-        ) + 1; 
-        // Convert to range 1 to 100, evenly disbursed.  Then
-        // shrink it down to the 1 to 5 range.
+    var r5 = Number.MAX_VALUE;
+    while( r5 > 5 ) {
+      r5 = rand7();
+    } // Only allow in the 1 to 5 range.
 
+    // { Random-Number : Count }
     r5Results[r5] = ++r5Results[r5] || 1;
 
   }
+
   console.log( "rand5viaRand7() results in SampleSize of " + sampleSize + ":\nNUM : COUNT");
-  for (var elem in r5Results) {
-    console.log( elem + " : " + r5Results[elem]);
-  }
 
   return r5Results;
 };
 
 console.log("RUNNNING rand5viaRand7()");
-rand5viaRand7(1000);
+console.log("", rand5viaRand7(1000) );
 console.log("rand5viaRand7() complete.");
 
 

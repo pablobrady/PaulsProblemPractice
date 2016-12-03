7cf400b5cafa0a13e172762e999353728904521a
Paul Brady <pablo.brady@gmail.com>
2016-12-03 14:37:34 -0800

    Adds nodes.

--- llistKtoLast/javascript.js
+++ llistKtoLast/javascript.js
@@ -1,5 +1,48 @@
 var answerStr = "n/a";
 
+// Make the LList
+var LList = function() {
+	this.homeNode = null;
+}
+
+LList.prototype.add = function( value ) {
+	var currNode = null;
+	var newNode = {
+		value: value,
+		next: null		/* only for clarity */
+	}
+
+	if( !this.homeNode ) {
+		this.homeNode = newNode;
+		return this.homeNode.value;
+	}
+
+	currNode = this.homeNode;
+	while( currNode.next ) {
+		currNode = currNode.next;
+	}
+	currNode.next = newNode;
+	currNode      = newNode;
+
+	return currNode.value;
+}
+
+
+
+// Init
+var xNode = new LList();
+for(var x=0; x<20; x++) {
+	console.log( "Add returned: " + xNode.add( x ) );
+}
+
+// Traverse all nodes
+
+
+// Output Last Node - 7
+
+
+
+
 var el = document.getElementById('answer');
 el.innerHTML = answerStr;
 
